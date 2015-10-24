c03fbb6995d0f5c9bdb15599251adac5a2a3b714
Sanam Narula <s5narula@uwaterloo.ca>
2015-10-24 00:43:53 -0400

    updates the commenting

--- cloudapp-mp3/src/TopWordFinderTopologyPartA.java
+++ cloudapp-mp3/src/TopWordFinderTopologyPartA.java
@@ -24,20 +24,6 @@ public class TopWordFinderTopologyPartA {
     Config config = new Config();
     config.setDebug(true);
 
-
-    /*
-    ----------------------TODO-----------------------
-    Task: wire up the topology
-
-    NOTE:make sure when connecting components together, using the functions setBolt(name,…) and setSpout(name,…),
-    you use the following names for each component:
-
-    RandomSentanceSpout -> "spout"
-    SplitSentenceBolt -> "split"
-    WordCountBolt -> "count"
-
-
-    ------------------------------------------------- */
 	builder.setSpout("spout", new RandomSentenceSpout(), 5);
 	builder.setBolt("split", new SplitSentenceBolt(), 8).shuffleGrouping("spout");
 	builder.setBolt("count", new WordCountBolt(), 12).fieldsGrouping("split", new Fields("word"));
